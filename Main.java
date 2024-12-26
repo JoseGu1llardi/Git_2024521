679ffe16a54b348fd8883dda3f13c8aa44cd7cf7
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2024-12-26 21:43:34 +0000

    added output

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jose Wellington Ribeiro - 2024521");
+        System.out.println("Jose Wellington Ribeiro - 2024521.");
     }
 }
